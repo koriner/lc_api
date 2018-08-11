3876b9791798f8fd29dce699f3b1fa854c885457
Shane Korin <sk@distriqt.com>
2018-08-11 12:25:01 +1000

    Load other related data for properties endpoint results

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,7 +3,24 @@ class PropertiesController < ApplicationController
   # GET /properties
   def index
     @props = Property.all
-    json_response(@props)
+    @results = []
+
+    @props.each do |prop|
+      result = prop.attributes
+
+      # add other properties from related things
+      address = Address.find_by_property_id(prop.full_property_id)
+      result[:full_address] = address.full_address
+      result[:postcode] = address.postcode
+      result[:state] = address.state
+      
+      lga = Lga.find_by_code(prop.lga_code)
+      result[:council] = lga.name
+
+      @results << result
+    end
+
+    json_response(@results)
   end
 
   # GET /property/:id
